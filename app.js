021116f01e75999ca0464f2b89e85e4425569126
= <=>
2023-03-07 13:27:34 +0100

    Ajout du OrbitControlers

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+// construction de la scène
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls( camera, renderer.domElement )
+camera.position.set( 200, 0, 0);
+controls.update();
+// le sphere
+
+const geometry = new THREE.SphereGeometry( 35, 32, 32);
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+function animate() {
+
+    //boucle pour animer automatiquement
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+
+}
+animate();
