659fd1509202a77c5609ad0a4ba5f7b2d10f4d83
= <=>
2023-03-08 14:44:51 +0100

    Ajout de l'orbitControls

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene(); 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 200, 0, 0 );
+controls.update();
 
+
+//le sphere
+const geometry = new THREE.SphereGeometry( 50, 32, 32 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
 
 const renderer = new THREE.WebGLRenderer(); 
 renderer.setSize( window.innerWidth, window.innerHeight ); 
 document.body.appendChild( renderer.domElement );
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+
+}
+
+animate();
