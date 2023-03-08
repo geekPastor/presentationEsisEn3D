a507aa462a94f8471d335a055315a0b1e3e7f611
= <=>
2023-03-08 18:44:00 +0100

    import de texture

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 import * as THREE from 'three';
+import { TextureLoader } from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene(); 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const controls = new OrbitControls( camera, renderer.domElement );
-camera.position.set( 200, 0, 0 );
+camera.position.set( 1, 0, 0 );
 controls.update();
 
 
 //le sphere
 const geometry = new THREE.SphereGeometry( 50, 32, 32 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const textureL = new THREE.TextureLoader();
+const texture = textureL.load('img360.jpg');
+const material = new THREE.MeshBasicMaterial( {
+    map: texture,
+    side: THREE.DoubleSide
+ } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
 
@@ -20,7 +26,6 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 function animate() {
-
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 
